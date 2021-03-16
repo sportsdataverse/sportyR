4b45cc368205762b49dcf10a46625f22ebd68663
Ross Drucker <ross.a.drucker@gmail.com>
2021-03-16 16:13:19 -0500

    Created a file to store mathematical functions, starting with the quadratic formula. Primarily useful for drawing baseball diamonds.

---
+++ R/math_functions.R
@@ -0,0 +1,23 @@
+#' Deploy the quadratic formula given inputs a, b, and c such that they satisfy the equation (a * (x ^ 2)) + bx + c = 0
+#'
+#' @param a The coefficient of the squared term
+#' @param b The coefficient of the linear term
+#' @param c The coefficient of the constant term
+#'
+#' @return The solutions to the equation
+quadratic_formula = function(a, b, c){
+  # The quadratic formula yields two solutions described by the formula:
+  # x = (-b +/- sqrt((b ^2) - (4 * a * c))) / (2 * a)
+
+  # First, find the solution using addition in the numerator
+  x_plus = (-b + sqrt((b ** 2) - (4 * a * c))) / (2 * a)
+
+  # Then, repeat using subtraction
+  x_minus = (-b - sqrt((b ** 2) - (4 * a * c))) / (2 * a)
+
+  # Combine the solutions into a single vector
+  solutions = c(x_plus, x_minus)
+
+  # Return the solution
+  return(solutions)
+}
