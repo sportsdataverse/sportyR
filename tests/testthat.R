7f52e51bd9cc22a678e7a3d7a843df1c9855481f
Ross Drucker <ross.a.drucker@gmail.com>
2021-03-24 19:41:44 -0500

    Re-added tests/ subdirectory to repository

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(sportyR)
+
+test_check("sportyR")
---
+++ tests/testthat/test-no-league-error.R
@@ -0,0 +1,17 @@
+# If no league is supplied, an error should be returned
+test_that("Error when league is not supplied", {
+  # Baseball
+  expect_error(geom_baseball())
+
+  # Basketball
+  expect_error(geom_basketball())
+
+  # Football
+  expect_error(geom_football())
+
+  # Hockey
+  expect_error(geom_hockey())
+
+  # Soccer
+  expect_error(geom_soccer())
+})
