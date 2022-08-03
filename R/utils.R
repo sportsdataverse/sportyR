76c0beb59b2b2528f43c025db9e07d9944eb0549
Ross Drucker <ross.a.drucker@gmail.com>
2022-08-03 15:50:45 -0500

    Created the %or% function, which is similar to Python's dict.get() method

---
+++ R/utils.R
@@ -0,0 +1,28 @@
+#' Set the default value when not provided in a list
+#'
+#' @param spec_val The specified value to use (assuming it's provided)
+#' @param default_val The value to use as the default
+#'
+#' @return A value dependent on if the left-hand value is provided, or the
+#'   default value specified on the right hand side
+#'
+#' @keywords internal
+`%or%` <- function(spec_val, default_val) {
+  cmp <- function(spec_val, default_val) {
+      if (identical(spec_val, FALSE) ||
+          is.null(spec_val) ||
+          is.na(spec_val) ||
+          is.nan(spec_val) ||
+          length(spec_val) == 0) {
+      return(default_val)
+    } else {
+      return(spec_val)
+    }
+  }
+
+  if (length(spec_val) > 1) {
+    mapply(cmp, spec_val, default_val)
+  } else {
+    cmp(spec_val, default_val)
+  }
+}
