7db102237d0745e807349876cb803b7578ec211e
Ross Drucker <ross.a.drucker@gmail.com>
2022-08-03 15:43:41 -0500

    Updated to conform to new style conventions

--- R/unit-conversions.R
+++ R/unit-conversions.R
@@ -14,16 +14,16 @@
 #' @examples
 #' convert_units(1, "in", "cm")
 #' convert_units(100, "cm", "m")
-convert_units = function(meas,
-                         from_unit,
-                         to_unit,
-                         conversion_columns = NULL) {
+convert_units <- function(meas,
+                          from_unit,
+                          to_unit,
+                          conversion_columns = NULL) {
   # Convert the from_unit and to_unit to be lower case
-  from_unit = tolower(from_unit)
-  to_unit = tolower(to_unit)
+  from_unit <- tolower(from_unit)
+  to_unit <- tolower(to_unit)
 
   # Create the conversion table
-  conversion_table = data.frame(
+  conversion_table <- data.frame(
     unit_name = c(
       "millimeters",
       "centimeters",
@@ -50,16 +50,20 @@ convert_units = function(meas,
     )
   )
 
-  # Ensure that the from_unit and to_unit exist somewhere in the conversion table
-  if (!(from_unit %in% conversion_table$unit_abbreviation) & !(from_unit %in% conversion_table$unit_name)) {
+  # Ensure that the from_unit and to_unit exist somewhere in the conversion
+  # table
+  if (!(from_unit %in% conversion_table$unit_abbreviation) &&
+      !(from_unit %in% conversion_table$unit_name)) {
     stop(glue::glue("{from_unit} is not a viable unit at this time."))
   }
 
-  if (!(to_unit %in% conversion_table$unit_abbreviation) & !(to_unit %in% conversion_table$unit_name)) {
+  if (!(to_unit %in% conversion_table$unit_abbreviation) &&
+      !(to_unit %in% conversion_table$unit_name)) {
     stop(glue::glue("{to_unit} is not a viable unit at this time."))
   }
 
-  # This function should work over entire data frames as well as individual vectors
+  # This function should work over entire data frames as well as individual
+  # vectors
   if (is.data.frame(meas)) {
 
     # If no columns are supplied, alert user of error
@@ -71,62 +75,120 @@ convert_units = function(meas,
     for (column in conversion_columns) {
       # Check to ensure the column name is in the data frame's column names
       if (!(column %in% names(meas))) {
-        message(glue::glue("{column} is not a column in the supplied data frame. Skipping..."))
-      }
-      # Columns must be either integer or numeric in type
-      else if (class(meas[, column]) %in% c("integer", "numeric")) {
-        if (from_unit %in% conversion_table$unit_abbreviation & to_unit %in% conversion_table$unit_abbreviation) {
-          meas[column] = meas[column] / conversion_table[conversion_table$unit_abbreviation == from_unit, "conversion_factor"]
-          meas[column] = meas[column] * conversion_table[conversion_table$unit_abbreviation == to_unit, "conversion_factor"]
+        message(
+          glue::glue(
+            "{column} is not a column in the supplied data frame. Skipping..."
+          )
+        )
+      } else if (class(meas[, column]) %in% c("integer", "numeric")) {
+        # Columns must be either integer or numeric in type
+        if (from_unit %in% conversion_table$unit_abbreviation &&
+            to_unit %in% conversion_table$unit_abbreviation) {
+          meas[column] <- meas[column] /
+            conversion_table[
+              conversion_table$unit_abbreviation == from_unit,
+              "conversion_factor"
+            ]
+          meas[column] <- meas[column] *
+            conversion_table[
+              conversion_table$unit_abbreviation == to_unit,
+              "conversion_factor"
+            ]
+        } else if (from_unit %in% conversion_table$unit_abbreviation &&
+                   to_unit %in% conversion_table$unit_name) {
+          meas[column] <- meas[column] /
+            conversion_table[
+              conversion_table$unit_abbreviation == from_unit,
+              "conversion_factor"
+            ]
+          meas[column] <- meas[column] *
+            conversion_table[
+              conversion_table$unit_name == to_unit,
+              "conversion_factor"
+            ]
+        } else if (from_unit %in% conversion_table$unit_name &&
+                   to_unit %in% conversion_table$unit_abbreviation) {
+          meas[column] <- meas[column] /
+            conversion_table[
+              conversion_table$unit_name == from_unit,
+              "conversion_factor"
+            ]
+          meas[column] <- meas[column] *
+            conversion_table[
+              conversion_table$unit_abbreviation == to_unit,
+              "conversion_factor"
+            ]
+        } else {
+          meas[column] <- meas[column] /
+            conversion_table[
+              conversion_table$unit_name == from_unit,
+              "conversion_factor"
+            ]
+          meas[column] <- meas[column] *
+            conversion_table[
+              conversion_table$unit_name == to_unit,
+              "conversion_factor"
+            ]
         }
-
-        else if (from_unit %in% conversion_table$unit_abbreviation & to_unit %in% conversion_table$unit_name) {
-          meas[column] = meas[column] / conversion_table[conversion_table$unit_abbreviation == from_unit, "conversion_factor"]
-          meas[column] = meas[column] * conversion_table[conversion_table$unit_name == to_unit, "conversion_factor"]
-        }
-
-        else if (from_unit %in% conversion_table$unit_name & to_unit %in% conversion_table$unit_abbreviation) {
-          meas[column] = meas[column] / conversion_table[conversion_table$unit_name == from_unit, "conversion_factor"]
-          meas[column] = meas[column] * conversion_table[conversion_table$unit_abbreviation == to_unit, "conversion_factor"]
-        }
-
-        else {
-          meas[column] = meas[column] / conversion_table[conversion_table$unit_name == from_unit, "conversion_factor"]
-          meas[column] = meas[column] * conversion_table[conversion_table$unit_name == to_unit, "conversion_factor"]
-        }
-      }
-
-      # If a supplied column is neither of type integer or numeric, alert user that the column is
-      else {
+      } else {
+        # If a supplied column is neither of type integer or numeric, alert user
+        # that the column is
         message(glue::glue("{column} is not convertable. Skipping..."))
       }
     }
 
     # Return the data frame
     return(meas)
-  }
-
-  else {
+  } else {
     # Perform the conversion by first converting the input into the base unit,
     # then converting to the desired unit
-    if (from_unit %in% conversion_table$unit_abbreviation & to_unit %in% conversion_table$unit_abbreviation) {
-      reduced = meas / conversion_table[conversion_table$unit_abbreviation == from_unit, "conversion_factor"]
-      result = reduced * conversion_table[conversion_table$unit_abbreviation == to_unit, "conversion_factor"]
-    }
-
-    else if (from_unit %in% conversion_table$unit_abbreviation & to_unit %in% conversion_table$unit_name) {
-      reduced = meas / conversion_table[conversion_table$unit_abbreviation == from_unit, "conversion_factor"]
-      result = reduced * conversion_table[conversion_table$unit_name == to_unit, "conversion_factor"]
-    }
-
-    else if (from_unit %in% conversion_table$unit_name & to_unit %in% conversion_table$unit_abbreviation) {
-      reduced = meas / conversion_table[conversion_table$unit_name == from_unit, "conversion_factor"]
-      result = reduced * conversion_table[conversion_table$unit_abbreviation == to_unit, "conversion_factor"]
-    }
-
-    else {
-      reduced = meas / conversion_table[conversion_table$unit_name == from_unit, "conversion_factor"]
-      result = reduced * conversion_table[conversion_table$unit_name == to_unit, "conversion_factor"]
+    if (from_unit %in% conversion_table$unit_abbreviation &&
+        to_unit %in% conversion_table$unit_abbreviation) {
+      reduced <- meas /
+        conversion_table[
+          conversion_table$unit_abbreviation == from_unit,
+          "conversion_factor"
+        ]
+      result <- reduced *
+        conversion_table[
+          conversion_table$unit_abbreviation == to_unit,
+          "conversion_factor"
+        ]
+    } else if (from_unit %in% conversion_table$unit_abbreviation &&
+               to_unit %in% conversion_table$unit_name) {
+      reduced <- meas /
+        conversion_table[
+          conversion_table$unit_abbreviation == from_unit,
+          "conversion_factor"
+        ]
+      result <- reduced *
+        conversion_table[
+          conversion_table$unit_name == to_unit,
+          "conversion_factor"
+        ]
+    } else if (from_unit %in% conversion_table$unit_name &&
+               to_unit %in% conversion_table$unit_abbreviation) {
+      reduced <- meas /
+        conversion_table[
+          conversion_table$unit_name == from_unit,
+          "conversion_factor"
+        ]
+      result <- reduced *
+        conversion_table[
+          conversion_table$unit_abbreviation == to_unit,
+          "conversion_factor"
+        ]
+    } else {
+      reduced <- meas /
+        conversion_table[
+          conversion_table$unit_name == from_unit,
+          "conversion_factor"
+        ]
+      result <- reduced *
+        conversion_table[
+          conversion_table$unit_name == to_unit,
+          "conversion_factor"
+        ]
     }
 
     # Return the converted measurement
